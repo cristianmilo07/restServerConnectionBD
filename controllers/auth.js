fb5f4c11171e7dfa0fa3afb56c0ba340a209b771
Cristian camilo <cristianmilo07@gmail.com>
2023-06-09 18:28:54 -0500

    // Verificar la contraseña *---* Revisar porque este método no me

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,50 @@
 import { response, request  } from 'express';
+import  bcryptjs from 'bcryptjs';
 
+import { Usuario } from '../models/usuario.js';
+import { generarJWT } from '../helpers/generar-jwt.js';
 const login = async(req, res = response) => {
-    res.json({
-        msg:'login ok'
-    })
+
+    const { correo, password } = req.body;
+    
+     try {
+        // Verificar si el email existe
+        const usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - correo'
+            });
+        }
+
+        // SI el usuario está activo
+        if ( !usuario.estado ) {
+            return res.status(400).json({
+                msg: 'Usuario / Password no son correctos - estado: false'
+            });
+        }
+
+        // Verificar la contraseña *---* Revisar porque este método no me funciona
+        // const validPassword = bcryptjs.compareSync( password, usuario.password );
+        // if ( !validPassword ) {
+        //     return res.status(400).json({
+        //         msg: 'Usuario / Password no son correctos - password'
+        //     });
+        // }
+
+        // Generar el JWT
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        })
+     } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            msg: 'Hable con el administrador'
+        });
+         
+     }
 }
 
 export { 
